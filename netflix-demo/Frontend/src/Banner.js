8bef61846601e6b47a765f6256016cff7f15c962
ben.strube <benjamin.strube@ust.com>
2021-11-20 00:57:49 -0500

    updating banner css

--- netflix-demo/Frontend/src/Banner.js
+++ netflix-demo/Frontend/src/Banner.js
@@ -46,6 +46,7 @@ function Banner() {
             <h1 className="banner_description">
                 {truncate(movie?.overview, 150)}
             </h1>
+            <div className="banner_fadebottom"/>
             </div>
         </header>
     )
