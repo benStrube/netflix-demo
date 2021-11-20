bf0c0fa313e661ba9c058b70a8e3ed0b28c16e98
ben.strube <benjamin.strube@ust.com>
2021-11-19 23:33:25 -0500

    cleaned up app formatting

--- netflix-demo/Frontend/src/App.js
+++ netflix-demo/Frontend/src/App.js
@@ -7,15 +7,41 @@ import requests from './requests';
 function App() {
   return (
     <div className="App">
-      <h1>Netflix Demo Front End</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={reuqests.fetchNetflixOriginals} />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      {/* Navbar */}
+      {/* Banner */}
+      <Row 
+        title="NETFLIX ORIGINALS" 
+        fetchUrl={reuqests.fetchNetflixOriginals} 
+        isLargeRow 
+      />
+      <Row 
+       title="Trending Now" 
+       fetchUrl={requests.fetchTrending} 
+      />
+      <Row 
+       title="Top Rated" 
+       fetchUrl={requests.fetchTopRated} 
+      />
+      <Row 
+       title="Action Movies" 
+       fetchUrl={requests.fetchActionMovies} 
+      />
+      <Row 
+       title="Comedy Movies" 
+       fetchUrl={requests.fetchComedyMovies} 
+      />
+      <Row 
+       title="Horror Movies" 
+       fetchUrl={requests.fetchHorrorMovies} 
+      />
+      <Row 
+       title="Romance Movies" 
+       fetchUrl={requests.fetchRomanceMovies} 
+      />
+      <Row 
+       title="Documentaries" 
+       fetchUrl={requests.fetchDocumentaries} 
+      />
     </div>
   );
 }
