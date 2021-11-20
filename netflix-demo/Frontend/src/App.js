08add576164a39a04e1cdd7225b3358b6f4267bc
ben.strube <benjamin.strube@ust.com>
2021-11-19 23:04:16 -0500

    added row css

--- netflix-demo/Frontend/src/App.js
+++ netflix-demo/Frontend/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './App.css';
+import Row from './Row';
+import reuqests from './requests';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
       <h1>Netflix Demo Front End</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={reuqests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
